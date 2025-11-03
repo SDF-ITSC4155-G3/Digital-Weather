1e72088ef2842c2d32bd1706080d3d982be83820
Brody Ehorn <brodyhehorn@gmail.com>
2025-11-02 22:01:02 -0500

    Added update_all_tile_counts which queries the databse for all entries, uses existing methods to figure which tile each device belongs to, and adds a numbers to the corresponding sections of the tile_dict

--- flask-server/heatmap_counter.py
+++ flask-server/heatmap_counter.py
@@ -1,6 +1,8 @@
-from coordinate_converter import GRID_SIZE
+from coordinate_converter import GRID_SIZE, coords_to_tile
+import sqlite3
 
 tile_dict = {}
+DB_PATH = "locations.db"
 
 for i in range(GRID_SIZE * GRID_SIZE):
     tile_dict[i] = 0
@@ -12,10 +14,42 @@ def increment_tile_count(tile_id):
 
 
 def get_tile_counts():
-    """Return a list of tile counts (length = GRID_SIZE * GRID_SIZE)"""
-    print("DEBUG: get_tile_counts() called. Current data:", tile_dict)  # 👈 for visibility
     return list(tile_dict.values())
+
+
+
+
+# Method should run increment_tile_count for each entry in the database
+def update_all_tile_counts():
+    conn = sqlite3.connect("locations.db")
+    cursor = conn.cursor()
+    cursor.execute("""
+        SELECT latitude, longitude
+        FROM UserLocation;
+    """)
+    results = cursor.fetchall()
+
+    for latitude, longitude in results:
+        increment_tile_count(coords_to_tile(latitude, longitude))
+
+    conn.close()
+
 
 def reset_tile_counts():
     for key in tile_dict:
         tile_dict[key] = 0
+
+def send_tile_counts():
+    reset_tile_counts()
+    update_all_tile_counts()
+    return get_tile_counts()
+
+
+
+
+
+
+
+
+
+
